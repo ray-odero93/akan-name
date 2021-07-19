3b2bc8f9b88eb5a2edd591fd5ba363ea9093d398
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-07-19 08:10:11 +0300

    Add variables by declaring bindings for day, month, year, formula, akan names and day of week.

---
+++ script.js
@@ -0,0 +1,11 @@
+// declaring bindings
+
+
+var CC, YY, MM, DD, d, dayOfWeek;
+var dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var femaleAkan = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleAkan = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+
+
+

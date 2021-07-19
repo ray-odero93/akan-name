45bfad69f2f686b24ca199b647af2b557072430f
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-07-19 08:13:39 +0300

    Define function that will validate the data submitted by user. also testing for specific scenarios and placing limits on the number of days and months the user can submit.

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ var maleAkan = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
 
 
+// this function will validate data submitted in the gender section of the form
+
+
+function validate() {
+  var gender = document.getElementById("gender");
+  if (document.form.day.value === " " || document.form.day.value > 31 || document.form.day.value <= 0) {
+    window.alert("Please provide a valid date!");
+    document.form.day.focus();
+    return false;
+  }
+
+  else if (document.form.month.value === " " || document.form.month.value > 12 || document.form.month.value <= 0) {
+    window.alert("Please provide a valid month!");
+    document.form.day.focus();
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+

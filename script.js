02764a1ca4ad043fc9aa78bd8ee5dd855224be35
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-07-23 08:34:44 +0300

    Add a variable to contain the outcome of the formula.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ $(document).ready(function () {
     var year = parseInt($("#year").val());
     var month = parseInt($("#month").val());
     var day = parseInt($("#day").val());
+    var gender = $("input:radio[name = gender]:checked").val();
+    var results = akanName(year, month, day, gender);
+    alert("Your Akan name is " + results);
+
     
   });
 });
